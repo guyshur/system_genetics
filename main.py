0658560c30088c3392436a01c84d45b528cd04a9
Guy Shur <guyshur@gmail.com>
2021-08-12 16:47:51 +0300

    regression

--- main.py
+++ main.py
@@ -54,15 +54,42 @@ inplace=True)
 
 phenotypes = [x for x in phenotypes_df.index if 'ethanol' in x.lower() and 'male' in x.lower() and 'female' not in x.lower()]
 phenotypes_df = phenotypes_df.loc[phenotypes,:]
-hypothalamus_expression_df_no_metadata = hypothalamus_expression_df[
+hypothalamus_expression_df = hypothalamus_expression_df[
     [column for column in hypothalamus_expression_df if column.startswith('BXD') and column.endswith('_M')]]
-liver_expression_df_no_metadata = liver_expression_df[
+liver_expression_df = liver_expression_df[
     [column for column in liver_expression_df if column.startswith('BXD') and column.endswith('_M')]]
-liver_expression_df_no_metadata.rename(
-    columns={strain: strain.split('_')[0] for strain in liver_expression_df_no_metadata.columns},
+liver_expression_df.rename(
+    columns={strain: strain.split('_')[0] for strain in liver_expression_df.columns},
 inplace=True)
-hypothalamus_expression_df_no_metadata.rename(
-    columns={strain: strain.split('_')[0] for strain in hypothalamus_expression_df_no_metadata.columns},
+hypothalamus_expression_df.rename(
+    columns={strain: strain.split('_')[0] for strain in hypothalamus_expression_df.columns},
 inplace=True)
 genotypes_numeric = genotypes_df.iloc[:,3:].applymap(lambda genotype: 0 if genotype == 'B' else 1 if genotype == 'H' else 2)
 
+genotype_liver_strains = genotypes_numeric.columns.intersection(liver_expression_df.columns)
+genotype_hypothalamus_strains = genotypes_numeric.columns.intersection(hypothalamus_expression_df.columns)
+liver_expression_df = liver_expression_df[genotype_liver_strains]
+hypothalamus_expression_df = hypothalamus_expression_df[genotype_hypothalamus_strains]
+
+# linear regression
+results: pd.DataFrame = \
+    genotypes_numeric[genotype_liver_strains].apply(lambda x: liver_expression_df.apply(lambda y: linregress(x, y)[3], axis=1),
+                              axis=1)
+# multiple test correction
+flattened_results = []
+for _, p_values in results.iterrows():
+    flattened_results.extend(p_values)
+bools, flattened_results = bh_procedure(flattened_results)
+num_genes = len(results.columns)
+for i in range(len(results.index)):
+    results.iloc[i, :] = flattened_results[i * num_genes:(i + 1) * num_genes]
+
+# boolean (is statistically significant) table
+is_significant: pd.DataFrame = results <= 0.05
+
+# dropping genes without eQTLs
+no_assoc_genes = []
+for gene in is_significant.columns:
+    if sum(is_significant.loc[:,gene]) == 0:
+        no_assoc_genes.append(gene)
+is_significant.drop(columns=no_assoc_genes,inplace=True)
