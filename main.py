b0ebf14696e8a99dd802a14db34b42755bf8e218
dhadas <dhadas@gmail.com>
2021-09-08 01:35:17 +0300

    Updated my notebook and adjust QTL_FDR_analysis function to general data.

--- main.py
+++ main.py
@@ -30,15 +30,21 @@ def run_QTL_analysis(genotypes_numeric, phenotypes_df):
 
     return pheno_vs_geno_df
 
-def fdr_QTL_analysis(QTL_results_df):
-    vec = QTL_results_df.reset_index().melt(id_vars=['Locus'], var_name='pheno', value_name='pval')
+def fdr_analysis(results_df):
+    index_prev_name = results_df.index.name
+    vec = results_df.copy()
+
+    vec.index = vec.index.set_names(['index'])
+    vec = vec.reset_index().melt(id_vars=['index'], var_name='cols', value_name='pval')
+
     vec_for_bh = vec[~vec.pval.isna()]
     vec_corrected = vec_for_bh.copy()
     bools, corrected = bh_procedure(vec_for_bh.pval)
     vec_corrected.pval = corrected
 
     # wide=vec.pivot(index='Locus', columns='pheno', values='pval')
-    wide_corrected = vec_corrected.pivot(index='Locus', columns='pheno', values='pval')
+    wide_corrected = vec_corrected.pivot(index='index', columns='cols', values='pval')
+    wide_corrected.index = wide_corrected.index.set_names([index_prev_name])
     return wide_corrected
 
 
@@ -100,163 +106,167 @@ def num_significant_genes_per_snp(df_without_weakly_associated_genes: pd.DataFra
             num_genes.append(0)
     return pd.Series(index=index,data=num_genes)
 
-hypothalamus_path = 'hypothalamus.txt'
-liver_path = 'liver.txt'
-genotypes_path = 'genotypes.xls'
-phenotypes_path = 'phenotypes.xls'
-genotypes_df: pd.DataFrame = pd.read_excel(genotypes_path)
-genotypes_df.columns = genotypes_df.loc[0, :]
-genotypes_df = genotypes_df.iloc[1:, :-3]
-genotypes_df = genotypes_df.set_index('Locus')
-hypothalamus_expression_df = pd.read_csv(hypothalamus_path,
-                                         sep='\t',
-                                         comment='#',
-                                         index_col=0,
-                                         )
-liver_expression_df = pd.read_csv(liver_path,
-                                         sep='\t',
-                                         comment='#',
-                                         index_col=0,
-                                         )
-phenotypes_df = pd.read_excel(phenotypes_path,index_col=1)
-phenotypes_df = phenotypes_df[phenotypes_df.columns[4:]]
+if __name__ == '__main__':
 
-liver_expression_df = preprocessing.preprocess_liver_data(liver_expression_df)
-hypothalamus_expression_df = preprocessing.preprocess_hypo_data(hypothalamus_expression_df)
+    hypothalamus_path = 'hypothalamus.txt'
+    liver_path = 'liver.txt'
+    genotypes_path = 'genotypes.xls'
+    phenotypes_path = 'phenotypes.xls'
+    genotypes_df: pd.DataFrame = pd.read_excel(genotypes_path)
+    genotypes_df.columns = genotypes_df.loc[0, :]
+    genotypes_df = genotypes_df.iloc[1:, :-3]
+    genotypes_df = genotypes_df.set_index('Locus')
+    hypothalamus_expression_df = pd.read_csv(hypothalamus_path,
+                                             sep='\t',
+                                             comment='#',
+                                             index_col=0,
+                                             )
+    liver_expression_df = pd.read_csv(liver_path,
+                                             sep='\t',
+                                             comment='#',
+                                             index_col=0,
+                                             )
+    phenotypes_df = pd.read_excel(phenotypes_path,index_col=1)
+    phenotypes_df = phenotypes_df[phenotypes_df.columns[4:]]
 
-curr = []
-redundant = []
-for index,row in genotypes_df.iterrows():
-    row = row.tolist()[3:]
-    if row == curr:
-        redundant.append(index)
+    liver_expression_df = preprocessing.preprocess_liver_data(liver_expression_df)
+    hypothalamus_expression_df = preprocessing.preprocess_hypo_data(hypothalamus_expression_df)
+
+    curr = []
+    redundant = []
+    for index,row in genotypes_df.iterrows():
+        row = row.tolist()[3:]
+        if row == curr:
+            redundant.append(index)
+        else:
+            curr = row
+    genotypes_df.drop(index=redundant,inplace=True)
+    liver_expression_df.rename(
+        columns={strain: '_'.join(strain.strip("'").split('_')[:2]) for strain in liver_expression_df.columns},
+    inplace=True)
+    liver_expression_df.rename(
+        columns={strain: '_'.join(strain.strip('"').split('_')[:2]) for strain in liver_expression_df.columns},
+    inplace=True)
+    phenotypes_df = phenotypes_df.loc[~phenotypes_df.index.duplicated(),:]
+    phenotypes = [x for x in phenotypes_df.index if 'blood' in x.lower() and 'male' in x.lower() and 'female' not in x.lower()]
+    phenotypes_df = phenotypes_df.loc[phenotypes,:]
+    print('num phenotypes: {}'.format(len(phenotypes)))
+
+    phenotypes_df.to_csv('data/phenotypes_filtered_df.csv')
+    hypothalamus_expression_df = hypothalamus_expression_df[
+        [column for column in hypothalamus_expression_df if column.startswith('BXD') and column.endswith('_M')]]
+    liver_expression_df = liver_expression_df[
+        [column for column in liver_expression_df if column.startswith('BXD') and column.endswith('_M')]]
+    liver_expression_df.rename(
+        columns={strain: strain.split('_')[0] for strain in liver_expression_df.columns},
+    inplace=True)
+    hypothalamus_expression_df.rename(
+        columns={strain: strain.split('_')[0] for strain in hypothalamus_expression_df.columns},
+    inplace=True)
+    genotypes_numeric = genotypes_df.iloc[:,3:].applymap(lambda genotype: 0 if genotype == 'B' else 1 if genotype == 'H' else 2)
+
+    genotype_liver_strains = genotypes_numeric.columns.intersection(liver_expression_df.columns)
+    genotype_hypothalamus_strains = genotypes_numeric.columns.intersection(hypothalamus_expression_df.columns)
+    liver_expression_df = liver_expression_df[genotype_liver_strains]
+    hypothalamus_expression_df = hypothalamus_expression_df[genotype_hypothalamus_strains]
+
+    try:
+        os.mkdir("./data/")
+    except FileExistsError:
+        pass
+
+    # Write intermediate files, after preprocessing
+
+    liver_expression_df.to_csv('./data/liver_expression_preproc_df.csv')
+    hypothalamus_expression_df.to_csv('./data/hypothalamus_expression_preproc_df.csv')
+    genotypes_df.to_csv('./data/genotypes_preproc_df.csv')
+    genotypes_numeric.to_csv('./data/genotypes_preproc_numeric_df.csv')
+
+
+
+    path = 'bin/qtl.pkl'
+    if os.path.isfile(path):
+            with open(path, 'rb') as f:
+                qtls = pickle.load(f)
     else:
-        curr = row
-genotypes_df.drop(index=redundant,inplace=True)
-liver_expression_df.rename(
-    columns={strain: '_'.join(strain.strip("'").split('_')[:2]) for strain in liver_expression_df.columns},
-inplace=True)
-liver_expression_df.rename(
-    columns={strain: '_'.join(strain.strip('"').split('_')[:2]) for strain in liver_expression_df.columns},
-inplace=True)
-phenotypes_df = phenotypes_df.loc[~phenotypes_df.index.duplicated(),:]
-phenotypes = [x for x in phenotypes_df.index if 'blood' in x.lower() and 'male' in x.lower() and 'female' not in x.lower()]
-phenotypes_df = phenotypes_df.loc[phenotypes,:]
-print('num phenotypes: {}'.format(len(phenotypes)))
-
-phenotypes_df.to_csv('data/phenotypes_filtered_df.csv')
-hypothalamus_expression_df = hypothalamus_expression_df[
-    [column for column in hypothalamus_expression_df if column.startswith('BXD') and column.endswith('_M')]]
-liver_expression_df = liver_expression_df[
-    [column for column in liver_expression_df if column.startswith('BXD') and column.endswith('_M')]]
-liver_expression_df.rename(
-    columns={strain: strain.split('_')[0] for strain in liver_expression_df.columns},
-inplace=True)
-hypothalamus_expression_df.rename(
-    columns={strain: strain.split('_')[0] for strain in hypothalamus_expression_df.columns},
-inplace=True)
-genotypes_numeric = genotypes_df.iloc[:,3:].applymap(lambda genotype: 0 if genotype == 'B' else 1 if genotype == 'H' else 2)
-
-genotype_liver_strains = genotypes_numeric.columns.intersection(liver_expression_df.columns)
-genotype_hypothalamus_strains = genotypes_numeric.columns.intersection(hypothalamus_expression_df.columns)
-liver_expression_df = liver_expression_df[genotype_liver_strains]
-hypothalamus_expression_df = hypothalamus_expression_df[genotype_hypothalamus_strains]
-
-try:
-    os.mkdir("./data/")
-except FileExistsError:
-    pass
-
-# Write intermediate files, after preprocessing
-
-liver_expression_df.to_csv('./data/liver_expression_preproc_df.csv')
-hypothalamus_expression_df.to_csv('./data/hypothalamus_expression_preproc_df.csv')
-genotypes_df.to_csv('./data/genotypes_preproc_df.csv')
-genotypes_numeric.to_csv('./data/genotypes_preproc_numeric_df.csv')
-
-
-
-path = 'bin/qtl.pkl'
-if os.path.isfile(path):
-        with open(path, 'rb') as f:
-            qtls = pickle.load(f)
-else:
-    common_geno_pheno = [x for x in phenotypes_df.columns if x in genotypes_numeric.columns]
-    shared_geno = genotypes_numeric.loc[:,common_geno_pheno]
-    shared_pheno = phenotypes_df.loc[:,common_geno_pheno]
-    assert shared_geno.columns.tolist() == shared_pheno.columns.tolist()
-    qtls = pd.DataFrame(index=shared_geno.index.tolist(), columns=shared_pheno.index.tolist(),dtype=float, data=np.zeros((
-        len(shared_geno.index), len(shared_pheno.index)
-    )))
-    assert all([isinstance(x, str) for x in shared_geno.index.tolist()])
-    assert all([isinstance(x, str) for x in shared_pheno.index.tolist()])
-    to_drop = set()
-    for snp, genotypes in shared_geno.iterrows():
-        for phenotype, phenotype_values in shared_pheno.iterrows():
-            mask = phenotype_values.notna()
-            assert phenotype_values.index.tolist() == genotypes.index.tolist()
-            phenotypes_cp = phenotype_values.copy()[mask]
-            genotypes_cp = genotypes.copy()[mask]
-            assert phenotypes_cp.index.tolist() == genotypes_cp.index.tolist()
-            try:
-                p_val = linregress(genotypes_cp, phenotypes_cp)[3]
+        common_geno_pheno = [x for x in phenotypes_df.columns if x in genotypes_numeric.columns]
+        shared_geno = genotypes_numeric.loc[:,common_geno_pheno]
+        shared_pheno = phenotypes_df.loc[:,common_geno_pheno]
+        assert shared_geno.columns.tolist() == shared_pheno.columns.tolist()
+        qtls = pd.DataFrame(index=shared_geno.index.tolist(), columns=shared_pheno.index.tolist(),dtype=float, data=np.zeros((
+            len(shared_geno.index), len(shared_pheno.index)
+        )))
+        assert all([isinstance(x, str) for x in shared_geno.index.tolist()])
+        assert all([isinstance(x, str) for x in shared_pheno.index.tolist()])
+        to_drop = set()
+        for snp, genotypes in shared_geno.iterrows():
+            for phenotype, phenotype_values in shared_pheno.iterrows():
+                mask = phenotype_values.notna()
+                assert phenotype_values.index.tolist() == genotypes.index.tolist()
+                phenotypes_cp = phenotype_values.copy()[mask]
+                genotypes_cp = genotypes.copy()[mask]
+                assert phenotypes_cp.index.tolist() == genotypes_cp.index.tolist()
                 try:
-                    assert 0 <= p_val <= 1
-                except AssertionError:
-                    print(p_val)
-                    raise AssertionError
-                assert isinstance(p_val, float)
-                qtls.at[snp,phenotype] = p_val
-            except FloatingPointError:  # at least one of the groups (B or D) is empty, can't do regression
-                print(phenotype)
-                to_drop.add(phenotype)
+                    p_val = linregress(genotypes_cp, phenotypes_cp)[3]
+                    try:
+                        assert 0 <= p_val <= 1
+                    except AssertionError:
+                        print(p_val)
+                        raise AssertionError
+                    assert isinstance(p_val, float)
+                    qtls.at[snp,phenotype] = p_val
+                except FloatingPointError:  # at least one of the groups (B or D) is empty, can't do regression
+                    print(phenotype)
+                    to_drop.add(phenotype)
 
-    qtls.drop(columns=list(to_drop),inplace=True)
-    assert not any(qtls.isna().values.flatten().tolist())
-    assert all([isinstance(float(x), float) for x in qtls.values.flatten().tolist()])
-    with open(path, 'wb+') as f:
-        pickle.dump(qtls, f)
-for x in qtls.index:
-    for y in qtls.columns:
-        try:
-            assert isinstance(qtls.at[x,y], float)
-        except AssertionError:
-            print(x,y)
-            print(qtls.at[x,y])
-            exit()
-multiple_test_correction(qtls)
-is_significant = qtls <= 0.05
+        qtls.drop(columns=list(to_drop),inplace=True)
+        assert not any(qtls.isna().values.flatten().tolist())
+        assert all([isinstance(float(x), float) for x in qtls.values.flatten().tolist()])
+        with open(path, 'wb+') as f:
+            pickle.dump(qtls, f)
 
-for snp in qtls.index:
-    for ph in qtls.columns:
-        if qtls.at[snp,ph] <= 0.05:
-            print(ph)
-            continue
-exit()
-QTL_results_not_corrected_df = run_QTL_analysis(genotypes_numeric, phenotypes_df)
-QTL_results_not_corrected_df.to_csv('./data/QTL_results_not_corrected_df.csv')
-QTL_results__corrected_df = fdr_QTL_analysis(QTL_results_not_corrected_df)
+    for x in qtls.index:
+        for y in qtls.columns:
+            try:
+                assert isinstance(qtls.at[x,y], float)
+            except AssertionError:
+                print(x,y)
+                print(qtls.at[x,y])
+                exit()
+    multiple_test_correction(qtls)
+    is_significant = qtls <= 0.05
 
-liver_eqtl_results = linear_regression(genotypes_numeric, liver_expression_df,
-                                       genotype_liver_strains, 'bin/liver_eqtl.pkl')
+    for snp in qtls.index:
+        for ph in qtls.columns:
+            if qtls.at[snp,ph] <= 0.05:
+                print(ph)
+                continue
+    QTL_results_not_corrected_df = run_QTL_analysis(genotypes_numeric, phenotypes_df)
+    QTL_results_not_corrected_df.to_csv('./data/QTL_results_not_corrected_df.csv')
+    QTL_results_corrected_df = fdr_analysis(QTL_results_not_corrected_df)
+    QTL_results_corrected_df.to_csv('./data/QTL_results_corrected_df.csv')
+    exit()
 
-multiple_test_correction(liver_eqtl_results)
-liver_significant: pd.DataFrame = liver_eqtl_results <= 0.05
-print('number of liver eQTLs: {}'.format(liver_significant.stack().value_counts()[True]))
-filter_genes_without_associations(liver_eqtl_results, liver_significant)
-num_of_significant_genes = num_significant_genes_per_snp(liver_eqtl_results)
-plt.hist(num_of_significant_genes,bins=30, label='liver eQTL distribution', alpha=0.5)
+    liver_eqtl_results = linear_regression(genotypes_numeric, liver_expression_df,
+                                           genotype_liver_strains, 'bin/liver_eqtl.pkl')
 
-hypothalamus_eqtl_results = linear_regression(genotypes_numeric, hypothalamus_expression_df,
-                                       genotype_hypothalamus_strains, 'bin/hypothalamus_eqtl.pkl')
-multiple_test_correction(hypothalamus_eqtl_results)
-hypothalamus_significant: pd.DataFrame = hypothalamus_eqtl_results <= 0.05
-print('number of hypothalamus eQTLs: {}'.format(hypothalamus_significant.stack().value_counts()[True]))
-filter_genes_without_associations(hypothalamus_eqtl_results, hypothalamus_significant)
-num_of_significant_genes = num_significant_genes_per_snp(hypothalamus_eqtl_results)
-plt.hist(num_of_significant_genes,bins=30,label='hypothalamus eQTL distribution',alpha=0.5)
-plt.legend()
-plt.title('Distribution of eQTLs')
-plt.xlabel('number of SNPs')
-plt.ylabel('number of significantly associated genes')
-plt.savefig('dist.png')
+    multiple_test_correction(liver_eqtl_results)
+    liver_significant: pd.DataFrame = liver_eqtl_results <= 0.05
+    print('number of liver eQTLs: {}'.format(liver_significant.stack().value_counts()[True]))
+    filter_genes_without_associations(liver_eqtl_results, liver_significant)
+    num_of_significant_genes = num_significant_genes_per_snp(liver_eqtl_results)
+    plt.hist(num_of_significant_genes,bins=30, label='liver eQTL distribution', alpha=0.5)
+
+    hypothalamus_eqtl_results = linear_regression(genotypes_numeric, hypothalamus_expression_df,
+                                           genotype_hypothalamus_strains, 'bin/hypothalamus_eqtl.pkl')
+    multiple_test_correction(hypothalamus_eqtl_results)
+    hypothalamus_significant: pd.DataFrame = hypothalamus_eqtl_results <= 0.05
+    print('number of hypothalamus eQTLs: {}'.format(hypothalamus_significant.stack().value_counts()[True]))
+    filter_genes_without_associations(hypothalamus_eqtl_results, hypothalamus_significant)
+    num_of_significant_genes = num_significant_genes_per_snp(hypothalamus_eqtl_results)
+    plt.hist(num_of_significant_genes,bins=30,label='hypothalamus eQTL distribution',alpha=0.5)
+    plt.legend()
+    plt.title('Distribution of eQTLs')
+    plt.xlabel('number of SNPs')
+    plt.ylabel('number of significantly associated genes')
+    plt.savefig('dist.png')
